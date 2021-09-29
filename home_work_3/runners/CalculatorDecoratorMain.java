003c44499263106b2068c9c02e6fef0a9a9362b5
aab <aab@mail.sru>
2021-09-29 09:13:15 +0300

    added update after deadline

--- home_work_3/runners/CalculatorDecoratorMain.java
+++ home_work_3/runners/CalculatorDecoratorMain.java
@@ -11,10 +11,28 @@ public class CalculatorDecoratorMain {
 
         ICalculator calculator = new CalculatorWithCounterAutoDecorator(new CalculatorWithMemoryDecorator(new CalculatorWithMathExtends()));
 
-        System.out.println(calculator instanceof CalculatorWithCounterAutoDecorator);
-        System.out.println(calculator instanceof CalculatorWithMemoryDecorator);
-        System.out.println(calculator instanceof CalculatorWithMathExtends);
+        if (calculator instanceof CalculatorWithCounterAutoDecorator) {
 
+            CalculatorWithCounterAutoDecorator calc = (CalculatorWithCounterAutoDecorator) calculator;
+
+            if (calc.getCalculator() instanceof CalculatorWithMemoryDecorator) {
+
+                CalculatorWithMemoryDecorator memoryCalc = (CalculatorWithMemoryDecorator) calc.getCalculator();
+
+                calc.divide(28, 5);
+                memoryCalc.writeInMemory();
+                calc.getDegree(memoryCalc.getMemory(), 2);
+                memoryCalc.writeInMemory();
+                calc.sum(4.1, memoryCalc.getMemory());
+                memoryCalc.writeInMemory();
+                calc.sum(memoryCalc.getMemory(), calc.multiply(15, 7));
+                memoryCalc.writeInMemory();
+                System.out.println("Было выполнено " + calc.getCountOperation() + " операций");
+                System.out.println("Последнее значение в памяти (результат всех вычислений): " + memoryCalc.getMemory());
+
+            }
+
+        }
 
     }
 
