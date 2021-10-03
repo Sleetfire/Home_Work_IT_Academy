3601e14ff85033c356889aa7cb1e828b8961b92d
SleetFire <41988252+Sleetfire@users.noreply.github.com>
2021-10-03 21:35:27 +0300

    deleted

---
+++ home_work_2/arrays/array23/TestMain.java
@@ -0,0 +1,16 @@
+package home_work_2.arrays.array23;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+
+     ForOperation forOperation = new ForOperation();
+
+     ForEachOperation forEachOperation = new ForEachOperation();
+
+     DoWhileOperation doWhileOperation = new DoWhileOperation();
+
+     WhileOperation whileOperation = new WhileOperation();
+
+    }
+}
