555dbae871d88fb6af577d5fc9b7632f178a5397
SleetFire <barkovskiy2000@gmail.com>
2021-12-03 13:48:45 +0300

    i hate this task

--- home_work_plus/strings_numerals/Main.java
+++ home_work_plus/strings_numerals/Main.java
@@ -3,7 +3,10 @@ package home_work_plus.strings_numerals;
 public class Main {
     public static void main(String[] args) {
         NumeralTranslator translator = new NumeralTranslator();
-        System.out.println(translator.toHoursMinuteSecondMillisecond(12345, false));
+        System.out.println(translator.toString(117));
+
+
+
 
     }
 }
--- home_work_plus/strings_numerals/NumeralTranslator.java
+++ home_work_plus/strings_numerals/NumeralTranslator.java
@@ -2,6 +2,7 @@ package home_work_plus.strings_numerals;
 
 import home_work_plus.strings_numerals.api.INumeralTranslator;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -9,7 +10,80 @@ public class NumeralTranslator implements INumeralTranslator {
 
     @Override
     public String toString(int number) {
-        return null;
+        List<Integer> stack = new ArrayList<>();
+        int temp;
+        while (number != 0) {
+            temp = number % 10;
+            stack.add(temp);
+            number /= 10;
+        }
+        StringBuilder builder = new StringBuilder();
+        Map<Integer, String> numeralsTable = getNumeralsTable();
+        int n = stack.size() - 1;
+        Collections.reverse(stack);
+        int currentNumber;
+        for (Integer integer : stack) {
+            if (integer != 0) {
+                currentNumber = integer * (int) Math.pow(10, n);
+
+                if (currentNumber > 10_000) {
+
+                    builder.append(numeralsTable.get(integer * 10)).append(" ").append(numeralsTable.get(1000)).append(" ");
+                } else if (currentNumber > 1000) {
+                    builder.append(numeralsTable.get(integer)).append(" ").append(numeralsTable.get(1000)).append(" ");
+                } else {
+                    builder.append(numeralsTable.get(currentNumber)).append(" ");
+                }
+                n--;
+            }
+        }
+
+        char firstSymbol = builder.charAt(0);
+        builder.setCharAt(0, Character.toUpperCase(firstSymbol));
+        return builder.toString();
+    }
+
+    private Map<Integer, String> getNumeralsTable() {
+        Map<Integer, String> numeralsMap = new HashMap<>();
+        numeralsMap.put(1, "один");
+        numeralsMap.put(2, "два");
+        numeralsMap.put(3, "три");
+        numeralsMap.put(4, "четыре");
+        numeralsMap.put(5, "пять");
+        numeralsMap.put(6, "шесть");
+        numeralsMap.put(7, "семь");
+        numeralsMap.put(8, "восемь");
+        numeralsMap.put(9, "девять");
+        numeralsMap.put(10, "десять");
+        numeralsMap.put(11, "одиннадцать");
+        numeralsMap.put(12, "двенадцать");
+        numeralsMap.put(13, "тринадцать");
+        numeralsMap.put(14, "четырнадцать");
+        numeralsMap.put(15, "пятнадцать");
+        numeralsMap.put(16, "шестнадцать");
+        numeralsMap.put(17, "семнадцать");
+        numeralsMap.put(18, "восемнадцать");
+        numeralsMap.put(19, "девятнадцать");
+        numeralsMap.put(20, "двадцать");
+        numeralsMap.put(30, "тридцать");
+        numeralsMap.put(40, "сорок");
+        numeralsMap.put(50, "пятьдесят");
+        numeralsMap.put(60, "шестьдесят");
+        numeralsMap.put(70, "семьдесят");
+        numeralsMap.put(80, "восемьдесят");
+        numeralsMap.put(90, "девяносто");
+        numeralsMap.put(100, "сто");
+        numeralsMap.put(200, "двести");
+        numeralsMap.put(300, "триста");
+        numeralsMap.put(400, "четыреста");
+        numeralsMap.put(500, "пятьсот");
+        numeralsMap.put(600, "шестьсот");
+        numeralsMap.put(700, "семьсот");
+        numeralsMap.put(800, "восемьсот");
+        numeralsMap.put(900, "девятьсот");
+        numeralsMap.put(1000, "тысяча");
+        numeralsMap.put(1000_000, "миллион");
+        return numeralsMap;
     }
 
     @Override
@@ -30,6 +104,7 @@ public class NumeralTranslator implements INumeralTranslator {
         } else {
             builder.append(weeks).append(" недель");
         }
+
         return builder.toString();
     }
 
@@ -41,7 +116,7 @@ public class NumeralTranslator implements INumeralTranslator {
         long updatedMilliseconds = millisecond - seconds * 1000 - minutes * 60_000 - hours * 3600_000;
         StringBuilder builder = new StringBuilder();
         if (shortFormat) {
-            if(hours < 10) {
+            if (hours < 10) {
                 builder.append("0").append(hours);
             } else {
                 builder.append(hours);
@@ -71,7 +146,7 @@ public class NumeralTranslator implements INumeralTranslator {
             if (hoursRemains == 1 && hours != 11) {
                 builder.append(hours).append(" час ");
             } else if (hoursRemains == 2 && hours % 100 != 12 || hoursRemains == 3 && hours % 100 != 13 ||
-                    hoursRemains == 4 && hours % 100 != 14){
+                    hoursRemains == 4 && hours % 100 != 14) {
                 builder.append(hours).append(" часа ");
             } else {
                 builder.append(hours).append(" часов ");
@@ -81,7 +156,7 @@ public class NumeralTranslator implements INumeralTranslator {
             if (minutesRemains == 1 && minutes != 11) {
                 builder.append(minutes).append(" минута ");
             } else if (minutesRemains == 2 && minutes % 100 != 12 || minutesRemains == 3 && minutes % 100 != 13 ||
-                    minutesRemains == 4 && minutes % 100 != 14){
+                    minutesRemains == 4 && minutes % 100 != 14) {
                 builder.append(minutes).append(" минуты ");
             } else {
                 builder.append(minutes).append(" минут ");
@@ -91,7 +166,7 @@ public class NumeralTranslator implements INumeralTranslator {
             if (secondRemains == 1 && seconds != 11) {
                 builder.append(seconds).append(" секунда ");
             } else if (secondRemains == 2 && seconds % 100 != 12 || secondRemains == 3 && seconds % 100 != 13 ||
-                    secondRemains == 4 && seconds % 100 != 14){
+                    secondRemains == 4 && seconds % 100 != 14) {
                 builder.append(seconds).append(" секунды ");
             } else {
                 builder.append(seconds).append(" секунд ");
@@ -101,7 +176,7 @@ public class NumeralTranslator implements INumeralTranslator {
             if (millisecondRemains == 1 && updatedMilliseconds != 11) {
                 builder.append(updatedMilliseconds).append(" миллисекунда");
             } else if (millisecondRemains == 2 && updatedMilliseconds % 100 != 12 || millisecondRemains == 3 &&
-                    updatedMilliseconds % 100 != 13 || millisecondRemains == 4 && updatedMilliseconds % 100 != 14){
+                    updatedMilliseconds % 100 != 13 || millisecondRemains == 4 && updatedMilliseconds % 100 != 14) {
                 builder.append(updatedMilliseconds).append(" миллисекунды");
             } else {
                 builder.append(updatedMilliseconds).append(" миллисекунд");
